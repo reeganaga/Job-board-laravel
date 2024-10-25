1656130c2372b26e0879fc03814e59a622c20285
rega <regacg@gmail.com>
2024-10-25 15:58:52 +0700

    [rega] add tag component and adjust career info

--- resources/views/career/index.blade.php
+++ resources/views/career/index.blade.php
@@ -2,6 +2,29 @@
     @foreach ($careers as $career)
         <x-card class="mb-4">
             {{ $career->title }}
+            <div class="mb-4 flex justify-between">
+                <h2 class="text-lg font-medium">{{ $career->title }}</h2>
+                <div class="text-slate-500">
+                    $ {{ number_format($career->salary) }}
+                </div>
+            </div>
+            <div class="mb-4 flex justify-between items-center text-sm text-slate-500">
+                <div class="flex space-x-4">
+                    <div>Company name</div>
+                    <div>{{ $career->location }}</div>
+                </div>
+                <div class="flex space-x-1">
+                    <x-tag>
+                        {{ $career->experience }}
+                    </x-tag>
+                    <x-tag>
+                        {{ $career->category }}
+                    </x-tag>
+                </div>
+            </div>
+            <p class="text-sm text-slate-500">
+                {!! nl2br(e($career->description)) !!}
+            </p>
         </x-card>
     @endforeach
 </x-layout>
