5de21dd3cbe004d97009b4b016e64a9dd46e4b84
rega <regacg@gmail.com>
2024-10-25 17:05:38 +0700

    [rega] breadcrumbs component

--- resources/views/career/index.blade.php
+++ resources/views/career/index.blade.php
@@ -1,4 +1,5 @@
 <x-layout>
+    <x-breadcrumbs class="mb-4" :links="['Careers' => route('careers.index')]" />
     @foreach ($careers as $career)
         <x-career-card :$career>
             <x-link-button :href="route('careers.show', $career)">Show</x-link-button>
--- resources/views/career/show.blade.php
+++ resources/views/career/show.blade.php
@@ -1,4 +1,5 @@
 <x-layout>
+    <x-breadcrumbs class="mb-4" :links="['Careers' => route('careers.index'), $career->title => '#']" />
     <x-career-card :$career />
 
 </x-layout>
