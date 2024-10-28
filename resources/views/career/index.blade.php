5693c38b51d7c89ca564d4e201766b2b5b336ac4
rega <regacg@gmail.com>
2024-10-28 09:48:06 +0700

    [rega] filter: add form tailwind plugin

--- resources/views/career/index.blade.php
+++ resources/views/career/index.blade.php
@@ -1,5 +1,21 @@
 <x-layout>
     <x-breadcrumbs class="mb-4" :links="['Careers' => route('careers.index')]" />
+    <x-card class="mb-4">
+        <div class="text-sm grid grid-cols-2 gap-4">
+            <div>
+                <div class="font-medium mb-1">Search</div>
+                <x-text-input name="search" placeholder="Search..." />
+            </div>
+            <div class="">
+                <div class="font-medium mb-1">Salary</div>
+                <div class="flex space-x-2">
+                    <x-text-input name="min_salary" value="" placeholder="Min Salary..." />
+                    <x-text-input name="max_salary" value="" placeholder="Max Salary..." />
+                </div>
+            </div>
+            <div>3</div>
+        </div>
+    </x-card>
     @foreach ($careers as $career)
         <x-career-card :$career>
             <x-link-button :href="route('careers.show', $career)">Show</x-link-button>
--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,8 @@
 use App\Http\Controllers\CareerController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('careers', CareerController::class)->only(['index','show']);
+Route::get('/', function () {
+    return redirect()->route('careers.index');
+});
+
+Route::resource('careers', CareerController::class)->only(['index', 'show']);
