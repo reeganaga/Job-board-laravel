4cf0d74f86bea3ed02d73bbed1626d2a76a79ba3
rega <regacg@gmail.com>
2024-10-29 13:32:14 +0700

    [rega] : sign in form

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,8 @@ Route::get('', function () {
     return to_route('careers.index');
 });
 
-Route::get('login', fn()=> to_route('Auth.create'))->name('login');
+Route::get('login', fn()=> to_route('auth.create'))->name('login');
 
 Route::resource('careers', CareerController::class)->only(['index', 'show']);
 
-Route::resource('Auth', AuthController::class)->only(['create','store']);
+Route::resource('auth', AuthController::class)->only(['create','store']);
