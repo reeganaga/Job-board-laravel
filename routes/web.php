83b4cd66e67caa741991af2160bc4dcc173688b0
rega <regacg@gmail.com>
2024-10-29 13:18:41 +0700

    [rega] : detail : add auth controller

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CareerController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return redirect()->route('careers.index');
+Route::get('', function () {
+    return to_route('careers.index');
 });
 
+Route::get('login', fn()=> to_route('Auth.create'))->name('login');
+
 Route::resource('careers', CareerController::class)->only(['index', 'show']);
+
+Route::resource('Auth', AuthController::class)->only(['create','store']);
